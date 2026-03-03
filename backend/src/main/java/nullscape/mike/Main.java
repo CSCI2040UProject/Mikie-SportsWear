32740ca3e7b7fced679a421463e1d6804cf33bf4
Rhys-de-Swart <180561892+Rhys-de-Swart@users.noreply.github.com>
2026-03-03 00:23:02 -0500

    Solved the browser security issue

--- backend/src/main/java/nullscape/mike/Main.java
+++ backend/src/main/java/nullscape/mike/Main.java
@@ -14,7 +14,6 @@ public class Main {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
         var helloContext = server.createContext("/api/helloworld/", new HelloWorldHandler());
-        helloContext.getFilters().add(new CorsFilter()); //do this instead for security bypass
 
         server.start();
         System.out.println("Server is listening on port 8080...");
@@ -33,28 +32,5 @@ class HelloWorldHandler implements HttpHandler {
         try (OutputStream os = exchange.getResponseBody()) {
             os.write(response.getBytes());
         }
-    }
-}
-
-//this is just the CORS security stuff moved to a class so that you don't have to put it in every single path
-//this is needed because browsers don't like talking to endpoints that don't match where it's getting the website from Eg localhost:8080 vs localhost:5173
-class CorsFilter extends Filter {
-    @Override
-    public String description() {
-        return "Adds CORS headers to responses";
-    }
-
-    @Override
-    public void doFilter(HttpExchange exchange, Chain chain) throws IOException {
-        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-        exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type,Authorization");
-
-        if (exchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
-            exchange.sendResponseHeaders(204, -1);
-            return;
-        }
-
-        chain.doFilter(exchange);
     }
 }
