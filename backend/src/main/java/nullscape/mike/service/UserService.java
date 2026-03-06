bcdb9308a2eeb134b0c45fc910ffd87e98ddb069
Rhys-de-Swart <180561892+Rhys-de-Swart@users.noreply.github.com>
2026-03-06 10:22:44 -0500

    moved backend checking

--- backend/src/main/java/nullscape/mike/service/UserService.java
+++ backend/src/main/java/nullscape/mike/service/UserService.java
@@ -13,8 +13,26 @@ public class UserService {
     }
 
     public static User registerUser(String username, String password, boolean isAdmin) {
-        //TODO: filter the user inputs
+        int size = 0;
+        int numbers = 0;
+        int symbols = 0;
+        int upper = 0;
+        for(char a : username.toCharArray()){
+            size++;
+            if(Character.isDigit(a)){
+                numbers++;
+            }
+            if(!Character.isLetterOrDigit(a) || !Character.isWhitespace(a)){
+                symbols++;
+            }
+            if(Character.isUpperCase(a)){
+                upper++;
+            }
+        }
+        if ((size>=8)&&(numbers>=4)&&(symbols>=2)&&(upper>0)) {
             return UserRepository.addUser(username, password, isAdmin);
+        }
+        return null;
     }
 
 
