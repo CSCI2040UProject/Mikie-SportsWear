54fa5beac118795550b191cfb49a9065f5969454
Rhys-de-Swart <180561892+Rhys-de-Swart@users.noreply.github.com>
2026-03-07 00:07:20 -0500

    logout button and profile page

--- backend/src/main/java/nullscape/mike/service/UserService.java
+++ backend/src/main/java/nullscape/mike/service/UserService.java
@@ -29,9 +29,10 @@ public class UserService {
                 upper++;
             }
         }
-        if ((size>=8)&&(numbers>=4)&&(symbols>=2)&&(upper>0)) {
+        if ((size>=3)&&(numbers>=0)&&(symbols>=0)&&(upper>0)) { //changed for easier testing
             return UserRepository.addUser(username, password, isAdmin);
         }
+        System.out.println("the password is invalid");
         return null;
     }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,12 +4,13 @@ import Icon from '@mdi/react';
 import { mdiAccount } from '@mdi/js';
 
 const Header = ({username}) => {
+    const newUsername = username.username;
     return (
         <header className={styles.header}>
             <h1><Link to="/">Mikie</Link></h1>
                 <Link to="/profile/">
                     <div>
-                    <h3>{username ? username : "Profile"}</h3>
+                    <h3>{newUsername ? newUsername : "Profile"}</h3>
                     <Icon path={mdiAccount} size={1} />
                     </div>
                 </Link>
