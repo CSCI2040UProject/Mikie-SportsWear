b1dc6ef5811d912d80c678d50d00c53b85d2b1d2
Rhys-de-Swart <180561892+Rhys-de-Swart@users.noreply.github.com>
2026-03-10 00:26:52 -0400

    Item page

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -3,6 +3,7 @@ import ErrorPage from "./components/ErrorPage.jsx";
 import Layout from "./Layout.jsx";
 import Profile from "./components/Profile.jsx";
 import Catalog from "./components/Catalog.jsx";
+import Item from "./components/Item.jsx";
 
 const routes = [
     {
@@ -17,6 +18,10 @@ const routes = [
             {
                 index: true,
                 element: <Catalog />,
+            },
+            {
+                path: "item/:id",
+                element: <Item/>,
             },
             {
                 path: "profile",
