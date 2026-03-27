9fa6761775e7cd8ab3b2d90850133c05317758e5
abineshj12 <abinesh.jeevamohan@ontariotechu.net>
2026-03-27 08:36:12 -0400

    adding our logo into the main page

--- frontend/src/components/FrontPage.jsx
+++ frontend/src/components/FrontPage.jsx
@@ -4,8 +4,16 @@ export function FrontPage() {
 
     return (
         <>
+            <div style={{ textAlign: 'center', paddingTop: '2vh' }}>
             <h1 className={'nike'}>WELCOME TO MIKIE SPORTSWEAR</h1>
             <Link to={'/catalog/'}>Shop All items</Link>
+                <div style= {{imgAlign: 'center', paddingTop: '5vh'}}>
+
+                </div>
+            <Link to="/catalog/">
+                <img src="/Mikie.svg" alt="Mikie"  width={750} height={750} />
+            </Link>
+            </div>
         </>
     )
 }
