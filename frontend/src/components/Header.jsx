61242093bb03d0c9f21d77a6892a958d519dd163
abineshj12 <abinesh.jeevamohan@ontariotechu.net>
2026-03-26 23:38:50 -0400

    created a header for user to locatae what they want to choose

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router";
 import styles from "../styles/Header.module.css";
 import Icon from '@mdi/react';
 import { mdiAccount } from '@mdi/js';
+const NAV_LINKS = ['Men', 'Women', 'Kids', 'Shoes', 'Accessories'];
 
 const Header = ({username}) => {
     const newUsername = username.username;
@@ -10,7 +11,18 @@ const Header = ({username}) => {
             <Link to="/">
                 <img src="/Mikie.svg" alt="Mikie" />
             </Link>
-                <Link to="/profile/">
+            <nav className={styles.nav}>                          {/* ← new */}
+                {NAV_LINKS.map(link => (
+                    <Link
+                        key={link}
+                        to={`/catalog?category=${link}`}
+                        className={styles.navLink}
+                    >
+                        {link}
+                    </Link>
+                ))}
+            </nav>
+            <Link to="/profile/">
                     <div>
                     <h3>{newUsername ? ("Hi, " + newUsername) : "Log in"}</h3>
                     <Icon path={mdiAccount} size={1} />
